d8fb0348f401218cd0bfbf18fe337fd42c3ef70e
Luo Han <lhannnn@outlook.com>
2025-10-20 02:26:44 +0000

    写了list_task部分

--- todo2.py
+++ todo2.py
@@ -1,5 +1,24 @@
-to do list '超级困难组'
+#to do list '超级困难组'
+
+
+del list_tasks():
+    """用于查看当有哪些任务"""
+    try：
+        with open("todo.txt", "r") as f:
+            lines = f.readlines()
+        if not lines:
+            print("empty")
+            return
+        for i, line in enumerate(lines,1):
+            print(f"{i}. {line.strip()}")
+    except FileNotFoundError:
+        print("(no tasks yet)")
+
+
+
 #用户端
+
+
 
 #后台数据处理端
 #数据存储端
